3d1cb0c2fb0268afc5ec98ff2fc155d220c42af8
Shamsiddinkhuja Abdurakhimov <64980797+chebarash@users.noreply.github.com>
2026-01-08 00:16:37 +0500

    Move and update favicon and manifest assets
    Relocated favicon.ico from src/app to public, added multiple favicon and manifest files (PNG, SVG, webmanifest) to public, and updated layout.tsx to reference the new assets. This improves asset organization and supports a wider range of devices and platforms.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import { connectToDatabase } from "@/lib/db";
 import Footer from "@/components/Footer";
 import { authOptions } from "./api/auth/[...nextauth]/authOptions";
 import { Analytics } from "@vercel/analytics/next";
+import Head from "next/head";
 
 const sixCaps = Six_Caps({
   weight: "400",
@@ -36,6 +37,22 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <Analytics />
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className={`${sixCaps.variable} ${geologica.variable}`}>
         <AuthProvider session={session}>{children}</AuthProvider>
         <Footer />
